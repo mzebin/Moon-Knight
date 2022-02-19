ed6fa847c3463172ef4e6cdcf00ff4fc5877f5be
Mohammed Zebin <mohammed.zebin7@gmail.com>
2022-02-20 03:27:37 +0400

    Added Lualine

---
+++ lua/plugins/configs/lualine/init.lua
@@ -0,0 +1,83 @@
+local status_ok, lualine = pcall(require, "lualine")
+if not status_ok then
+    return
+end
+
+local hide_in_width = function()
+    return vim.fn.winwidth(0) > 80
+end
+
+local diagnostics = {
+    "diagnostics",
+    sources = {"nvim_diagnostic"},
+    sections = {"error", "warn"},
+    symbols = {error = " ", warn = " "},
+    colored = true,
+    update_in_insert = false,
+    always_visible = true,
+}
+
+local diff = {
+    "diff",
+    colored = false,
+    symbols = {added = " ", modified = " ", removed = " "},
+    cond = hide_in_width
+}
+
+local mode = {
+    "mode",
+    fmt = function(str)
+        return "-- " .. str .. " --"
+    end,
+}
+
+local filetype = {
+	"filetype",
+	icons_enabled = true,
+}
+
+local branch = {
+	"branch",
+	icons_enabled = true,
+	icon = "",
+}
+
+local location = {
+    "location",
+    padding = 0,
+}
+
+local spaces = function()
+	return "Spaces: " .. vim.api.nvim_buf_get_option(0, "shiftwidth")
+end
+
+lualine.setup({
+    options = {
+        icons_enabled = true,
+        theme = "auto",
+        component_separators = {left = "", right = ""},
+        section_separators = {left = "", right = ""},
+        disabled_filetypes = {"NvimTree", "alpha"},
+        always_divide_middle = true,
+    },
+	sections = {
+		lualine_a = {mode},
+		lualine_b = {branch, diagnostics},
+		lualine_c = {diff},
+
+		lualine_x = {spaces, "encoding"},
+		lualine_y = {filetype},
+		lualine_z = {location},
+	},
+	inactive_sections = {
+		lualine_a = {mode},
+		lualine_b = {branch, diagnostics},
+		lualine_c = {diff, "filename"},
+
+		lualine_x = {spaces, "encoding"},
+		lualine_y = {filetype},
+		lualine_z = {location},
+	},
+	tabline = {},
+	extensions = {},
+})
