ebb919f2c460b77194e28799e61ac22dc98b8a89
Mohammed Zebin <mohammed.zebin7@gmail.com>
2022-03-27 23:45:25 +0400

    Updated autocmds

--- lua/core/autocommands.lua
+++ lua/core/autocommands.lua
@@ -6,4 +6,9 @@ vim.cmd([[
     augroup end
 
     autocmd BufEnter * ++nested if winnr('$') == 1 && bufname() == 'NvimTree_' . tabpagenr() | quit | endif
+
+    augroup _colorscheme
+        autocmd!
+        autocmd ColorScheme * source ~/.config/nvim/lua/colors/highlights.lua
+    augroup end
 ]])
